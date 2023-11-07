476596574b4b427049930b6567cb093b7130dddc
Muhammad Asjad <m.asjad.ca@gmail.com>
2023-11-07 15:40:50 -0500

    Chapter 11 - Sorted GameObjects/Some part of Coding UI

--- Assets/Scripts/GameUI.cs
+++ Assets/Scripts/GameUI.cs
@@ -6,6 +6,27 @@ using UnityEngine.UI;
 public class GameUI : MonoBehaviour
 {
     [SerializeField]
+    private Text ammoText;
+    [SerializeField]
+    private Text healthText;
+    [SerializeField]
+    private Text armorText;
+    [SerializeField]
+    private Text scoreText;
+    [SerializeField]
+    private Text pickupText;
+    [SerializeField]
+    private Text waveText;
+    [SerializeField]
+    private Text enemyText;
+    [SerializeField]
+    private Text waveClearText;
+    [SerializeField]
+    private Text newWaveText;
+    [SerializeField]
+    Player player;
+
+    [SerializeField]
     Sprite redReticle;
     [SerializeField]
     Sprite yellowReticle;
@@ -30,5 +51,38 @@ public class GameUI : MonoBehaviour
             default:
                 return;
         }
+    }
+
+    void Start()
+    {
+        SetArmorText(player.armor);
+        SetHealthText(player.health);
+    }
+
+    public void SetArmorText(int armor)
+    {
+        armorText.text = "Armor: " + armor;
+    }
+
+    public void SetHealthText(int health)
+    {
+        healthText.text = "Health: " + health;
+    }
+
+    public void SetAmmoText(int ammo)
+    {
+        ammoText.text = "Ammo: " + ammo;
+    }
+    public void SetScoreText(int score)
+    {
+        scoreText.text = "" + score;
+    }
+    public void SetWaveText(int time)
+    {
+        waveText.text = "Next Wave: " + time;
+    }
+    public void SetEnemyText(int enemies)
+    {
+        enemyText.text = "Enemies: " + enemies;
     }
 }
