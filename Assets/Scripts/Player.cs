ea768c9ab16350cab3fd4d807494231a59cdc61a
Muhammad Asjad <m.asjad.ca@gmail.com>
2023-11-10 19:37:01 -0500

    Chapter 11 - Added Game Over Screen

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,8 @@ public class Player : MonoBehaviour
     public int health;
     public int armor;
     public GameUI gameUI;
+    public Game game;
+    public AudioClip playerDead;
     private GunEquipper gunEquipper;
     private Ammo ammo;
 
@@ -41,7 +43,8 @@ public class Player : MonoBehaviour
 
         if (health <= 0)
         {
-            UnityEngine.Debug.Log("GameOver");
+            GetComponent<AudioSource>().PlayOneShot(playerDead);
+            game.GameOver();
         }
     }
 
